4989c5bcea05d640ac9202cb6ce390571f05eb58
Nathan <mcelrobe@oregonstate.edu>
2025-05-01 16:58:25 +0000

    explain your changes, such as: lab1 problem2

---
+++ calculator.py
@@ -0,0 +1,17 @@
+
+
+def sum(a, b):
+    """Return the sum of two numbers."""
+    return a + b
+
+def product(a, b):
+    """Return the product of two numbers."""
+    return a * b
+
+def sqrt(x):
+    """Return the square root of a number."""
+    return x ** 0.5
+
+
+if __name__ == "__main__":
+    pass
